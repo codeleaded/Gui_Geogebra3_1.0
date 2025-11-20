5eecec3ca8cc7553fdc08158530f11f9784a1dd7
codeleaded <codeleaded@gmail.com>
2025-11-20 15:11:53 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -6,18 +6,18 @@
 
 mesh meshCube;
 
-mat4x4 matProj;
-vec3d vCamera = { 0.0f,0.0f,0.0f,1.0f };
-vec3d vVelocity = { 0.0f,0.0f,0.0f,1.0f };
-vec3d vLookDir;
+M4x4D matProj;
+Vec3D vCamera = { 0.0f,0.0f,0.0f,1.0f };
+Vec3D vVelocity = { 0.0f,0.0f,0.0f,1.0f };
+Vec3D vLookDir;
 float fYaw;	
 float fPitch;	
 float fTheta;
 
-vec3d vLength = { 0.5f,1.8f,0.5f,1.0f };
+Vec3D vLength = { 0.5f,1.8f,0.5f,1.0f };
 Vec2 MouseBefore = { 0.0f,0.0f };
 
-mat4x4 matView;
+M4x4D matView;
 
 int Mode = 0;
 int Menu = 0;
@@ -30,37 +30,37 @@ float Function_2D(float x,float y){
 }
 
 int compare(const void* e1,const void* e2) {
-	triangle t1 = *(triangle*)e1;
-	triangle t2 = *(triangle*)e2;
+	Tri3D t1 = *(Tri3D*)e1;
+	Tri3D t2 = *(Tri3D*)e2;
 	float z1 = (t1.p[0].z+t1.p[1].z+t1.p[2].z)/3;
     float z2 = (t2.p[0].z+t2.p[1].z+t2.p[2].z)/3;
     return z1 == z2 ? 0 : (z1 < z2 ? 1 : -1);
 }
 void Triangles_Project(){
-	Vector vecTrianglesToRaster = Vector_New(sizeof(triangle));
+	Vector vecTrianglesToRaster = Vector_New(sizeof(Tri3D));
 
 	for (int i = 0;i<meshCube.tris.size;i++){
-		triangle tri = *(triangle*)Vector_Get(&meshCube.tris,i);
+		Tri3D tri = *(Tri3D*)Vector_Get(&meshCube.tris,i);
 		
-		vec3d vCameraRay = vec3d_Sub(tri.p[0], vCamera);
+		Vec3D vCameraRay = Vec3D_Sub(tri.p[0], vCamera);
 
-		if (vec3d_DotProduct(tri.n,vCameraRay) < 0.0f){
+		if (Vec3D_DotProduct(tri.n,vCameraRay) < 0.0f){
 			tri.p[0] = Matrix_MultiplyVector(matView,tri.p[0]);
 			tri.p[1] = Matrix_MultiplyVector(matView,tri.p[1]);
 			tri.p[2] = Matrix_MultiplyVector(matView,tri.p[2]);
 
 			int nClippedTriangles = 0;
-			triangle clipped[2];
-			nClippedTriangles = Triangle_ClipAgainstPlane(vec3d_new(0.0f,0.0f,0.1f),vec3d_new(0.0f,0.0f,1.0f),tri,&clipped[0],&clipped[1]);
+			Tri3D clipped[2];
+			nClippedTriangles = Triangle_ClipAgainstPlane(Vec3D_new(0.0f,0.0f,0.1f),Vec3D_new(0.0f,0.0f,1.0f),tri,&clipped[0],&clipped[1]);
 
 			for (int n = 0; n < nClippedTriangles; n++){
 				clipped[n].p[0] = Matrix_MultiplyVector(matProj, clipped[n].p[0]);
 				clipped[n].p[1] = Matrix_MultiplyVector(matProj, clipped[n].p[1]);
 				clipped[n].p[2] = Matrix_MultiplyVector(matProj, clipped[n].p[2]);
 
-				clipped[n].p[0] = vec3d_Div(clipped[n].p[0], clipped[n].p[0].w);
-				clipped[n].p[1] = vec3d_Div(clipped[n].p[1], clipped[n].p[1].w);
-				clipped[n].p[2] = vec3d_Div(clipped[n].p[2], clipped[n].p[2].w);
+				clipped[n].p[0] = Vec3D_Div(clipped[n].p[0], clipped[n].p[0].w);
+				clipped[n].p[1] = Vec3D_Div(clipped[n].p[1], clipped[n].p[1].w);
+				clipped[n].p[2] = Vec3D_Div(clipped[n].p[2], clipped[n].p[2].w);
 
 				clipped[n].p[0].x *= -1.0f;
 				clipped[n].p[1].x *= -1.0f;
@@ -69,10 +69,10 @@ void Triangles_Project(){
 				clipped[n].p[1].y *= -1.0f;
 				clipped[n].p[2].y *= -1.0f;
 
-				vec3d vOffsetView = vec3d_new( 1,1,0 );
-				clipped[n].p[0] = vec3d_Add(clipped[n].p[0], vOffsetView);
-				clipped[n].p[1] = vec3d_Add(clipped[n].p[1], vOffsetView);
-				clipped[n].p[2] = vec3d_Add(clipped[n].p[2], vOffsetView);
+				Vec3D vOffsetView = Vec3D_new( 1,1,0 );
+				clipped[n].p[0] = Vec3D_Add(clipped[n].p[0], vOffsetView);
+				clipped[n].p[1] = Vec3D_Add(clipped[n].p[1], vOffsetView);
+				clipped[n].p[2] = Vec3D_Add(clipped[n].p[2], vOffsetView);
 				clipped[n].p[0].x *= 0.5f * (float)GetWidth();
 				clipped[n].p[0].y *= 0.5f * (float)GetHeight();
 				clipped[n].p[1].x *= 0.5f * (float)GetWidth();
@@ -89,10 +89,10 @@ void Triangles_Project(){
 
 	for (int i = 0;i<vecTrianglesToRaster.size;i++)
 	{
-		triangle triToRaster = *(triangle*)Vector_Get(&vecTrianglesToRaster,i);
+		Tri3D triToRaster = *(Tri3D*)Vector_Get(&vecTrianglesToRaster,i);
 
-		triangle clipped[2];
-		Vector listTriangles = Vector_New(sizeof(triangle));
+		Tri3D clipped[2];
+		Vector listTriangles = Vector_New(sizeof(Tri3D));
 
 		Vector_Push(&listTriangles,&triToRaster);
 		int nNewTriangles = 1;
@@ -102,16 +102,16 @@ void Triangles_Project(){
 			int nTrisToAdd = 0;
 			while (nNewTriangles > 0)
 			{
-				triangle test = *(triangle*)Vector_Get(&listTriangles,0);
+				Tri3D test = *(Tri3D*)Vector_Get(&listTriangles,0);
 				Vector_Remove(&listTriangles,0);
 				nNewTriangles--;
 
 				switch (p)
 				{
-				case 0:	nTrisToAdd = Triangle_ClipAgainstPlane(vec3d_new( 0.0f, 0.0f, 0.0f ), 					vec3d_new( 0.0f, 1.0f, 0.0f ), 	test, &clipped[0], &clipped[1]); break;
-				case 1:	nTrisToAdd = Triangle_ClipAgainstPlane(vec3d_new( 0.0f, (float)GetHeight() - 1, 0.0f ), vec3d_new( 0.0f, -1.0f, 0.0f ), test, &clipped[0], &clipped[1]); break;
-				case 2:	nTrisToAdd = Triangle_ClipAgainstPlane(vec3d_new( 0.0f, 0.0f, 0.0f ), 					vec3d_new( 1.0f, 0.0f, 0.0f ), 	test, &clipped[0], &clipped[1]); break;
-				case 3:	nTrisToAdd = Triangle_ClipAgainstPlane(vec3d_new( (float)GetWidth() - 1, 0.0f, 0.0f ), 	vec3d_new( -1.0f, 0.0f, 0.0f ), test, &clipped[0], &clipped[1]); break;
+				case 0:	nTrisToAdd = Triangle_ClipAgainstPlane(Vec3D_new( 0.0f, 0.0f, 0.0f ), 					Vec3D_new( 0.0f, 1.0f, 0.0f ), 	test, &clipped[0], &clipped[1]); break;
+				case 1:	nTrisToAdd = Triangle_ClipAgainstPlane(Vec3D_new( 0.0f, (float)GetHeight() - 1, 0.0f ), Vec3D_new( 0.0f, -1.0f, 0.0f ), test, &clipped[0], &clipped[1]); break;
+				case 2:	nTrisToAdd = Triangle_ClipAgainstPlane(Vec3D_new( 0.0f, 0.0f, 0.0f ), 					Vec3D_new( 1.0f, 0.0f, 0.0f ), 	test, &clipped[0], &clipped[1]); break;
+				case 3:	nTrisToAdd = Triangle_ClipAgainstPlane(Vec3D_new( (float)GetWidth() - 1, 0.0f, 0.0f ), 	Vec3D_new( -1.0f, 0.0f, 0.0f ), test, &clipped[0], &clipped[1]); break;
 				}
 
 				for (int w = 0; w < nTrisToAdd; w++)
@@ -121,7 +121,7 @@ void Triangles_Project(){
 		}
 
 		for (int j = 0;j<listTriangles.size;j++){
-			triangle t = *(triangle*)Vector_Get(&listTriangles,j);
+			Tri3D t = *(Tri3D*)Vector_Get(&listTriangles,j);
 			//RenderTriangle(((Vec2){ t.p[0].x, t.p[0].y }),((Vec2){ t.p[1].x, t.p[1].y }),((Vec2){ t.p[2].x, t.p[2].y }),t.c);
 			//RenderTriangleWire(((Vec2){ t.p[0].x, t.p[0].y }),((Vec2){ t.p[1].x, t.p[1].y }),((Vec2){ t.p[2].x, t.p[2].y }),WHITE,1.0f);
 
@@ -157,7 +157,7 @@ void Setup(AlxWindow* w){
 	RGA_Set(Time_Nano());
 	RGA_Get(6969);
 
-	meshCube = (mesh){ Vector_New(sizeof(triangle)) };
+	meshCube = (mesh){ Vector_New(sizeof(Tri3D)) };
 	matProj = Matrix_MakeProjection(90.0f, (float)GetHeight() / (float)GetWidth(), 0.1f, 1000.0f);
 }
 
@@ -201,38 +201,38 @@ void Update(AlxWindow* w){
 	else
 		vVelocity.y = 0.0f;
 
-	mat4x4 matCameraRot = Matrix_MakeRotationY(fYaw);
-	vec3d vForward = Matrix_MultiplyVector(matCameraRot,vec3d_new(0.0f,0.0f,1.0f));
-	vec3d vLeft = vec3d_Perp(vForward);
+	M4x4D matCameraRot = Matrix_MakeRotationY(fYaw);
+	Vec3D vForward = Matrix_MultiplyVector(matCameraRot,Vec3D_new(0.0f,0.0f,1.0f));
+	Vec3D vLeft = Vec3D_Perp(vForward);
 	
 	if(Stroke(ALX_KEY_W).DOWN){
-		vCamera = vec3d_Add(vCamera,vec3d_Mul(vForward,5.0f * w->ElapsedTime));
+		vCamera = Vec3D_Add(vCamera,Vec3D_Mul(vForward,5.0f * w->ElapsedTime));
 	}
 	if(Stroke(ALX_KEY_S).DOWN){
-		vCamera = vec3d_Add(vCamera,vec3d_Mul(vForward,-5.0f * w->ElapsedTime));
+		vCamera = Vec3D_Add(vCamera,Vec3D_Mul(vForward,-5.0f * w->ElapsedTime));
 	}
 	if(Stroke(ALX_KEY_A).DOWN){
-		vCamera = vec3d_Add(vCamera,vec3d_Mul(vLeft,-5.0f * w->ElapsedTime));
+		vCamera = Vec3D_Add(vCamera,Vec3D_Mul(vLeft,-5.0f * w->ElapsedTime));
 	}
 	if (Stroke(ALX_KEY_D).DOWN){
-		vCamera = vec3d_Add(vCamera,vec3d_Mul(vLeft,5.0f * w->ElapsedTime));
+		vCamera = Vec3D_Add(vCamera,Vec3D_Mul(vLeft,5.0f * w->ElapsedTime));
 	}
 
-	vVelocity = vec3d_Add(vVelocity,vec3d_Mul((vec3d){ 0.0f,0.0f,0.0f,1.0f },w->ElapsedTime));
-	vCamera = vec3d_Add(vCamera,vec3d_Mul(vVelocity,w->ElapsedTime));
+	vVelocity = Vec3D_Add(vVelocity,Vec3D_Mul((Vec3D){ 0.0f,0.0f,0.0f,1.0f },w->ElapsedTime));
+	vCamera = Vec3D_Add(vCamera,Vec3D_Mul(vVelocity,w->ElapsedTime));
 
 	float Border = F32_PI * 0.5f - 0.001f;
 	if(fPitch<-Border) fPitch = -Border;
 	if(fPitch>Border) fPitch = Border;
 
-	vec3d vUp = vec3d_new( 0.0f,1.0f,0.0f );
-	vec3d vTarget = vec3d_new( 0.0f,0.0f,1.0f );
-	mat4x4 matCameraRotX = Matrix_MakeRotationX(fPitch);
+	Vec3D vUp = Vec3D_new( 0.0f,1.0f,0.0f );
+	Vec3D vTarget = Vec3D_new( 0.0f,0.0f,1.0f );
+	M4x4D matCameraRotX = Matrix_MakeRotationX(fPitch);
 	vLookDir = Matrix_MultiplyVector(matCameraRotX,vTarget);
 	vLookDir = Matrix_MultiplyVector(matCameraRot,vLookDir);
 	
-	vTarget = vec3d_Add(vCamera, vLookDir);
-	mat4x4 matCamera = Matrix_PointAt(vCamera, vTarget, vUp);
+	vTarget = Vec3D_Add(vCamera, vLookDir);
+	M4x4D matCamera = Matrix_PointAt(vCamera, vTarget, vUp);
 	matView = Matrix_QuickInverse(matCamera);
 
 
@@ -244,25 +244,25 @@ void Update(AlxWindow* w){
 			const float y1 = FunctionOrigin.y + i;
 			const float y2 = FunctionOrigin.y + i+1;
 
-			const vec3d p1 = { x1,Function_2D(x1,y1),y1,1.0f };
-			const vec3d p2 = { x2,Function_2D(x2,y1),y1,1.0f };
-			const vec3d p3 = { x1,Function_2D(x1,y2),y2,1.0f };
-			const vec3d p4 = { x2,Function_2D(x2,y2),y2,1.0f };
+			const Vec3D p1 = { x1,Function_2D(x1,y1),y1,1.0f };
+			const Vec3D p2 = { x2,Function_2D(x2,y1),y1,1.0f };
+			const Vec3D p3 = { x1,Function_2D(x1,y2),y2,1.0f };
+			const Vec3D p4 = { x2,Function_2D(x2,y2),y2,1.0f };
 
-			triangle t1 = { p1,p2,p4,{},RED };
-			triangle t2 = { p1,p4,p3,{},RED };
-			triangle t3 = { p1,p4,p2,{},RED };
-			triangle t4 = { p1,p3,p4,{},RED };
+			Tri3D t1 = { p1,p2,p4,{},RED };
+			Tri3D t2 = { p1,p4,p3,{},RED };
+			Tri3D t3 = { p1,p4,p2,{},RED };
+			Tri3D t4 = { p1,p3,p4,{},RED };
 
 			Triangle_CalcNorm(&t1);
 			Triangle_CalcNorm(&t2);
 			Triangle_CalcNorm(&t3);
 			Triangle_CalcNorm(&t4);
 
-			Triangle_ShadeNorm(&t1,(vec3d){ 0.5f,0.4f,0.6f,1.0f });
-			Triangle_ShadeNorm(&t2,(vec3d){ 0.5f,0.4f,0.6f,1.0f });
-			Triangle_ShadeNorm(&t3,(vec3d){ 0.5f,0.4f,0.6f,1.0f });
-			Triangle_ShadeNorm(&t4,(vec3d){ 0.5f,0.4f,0.6f,1.0f });
+			Triangle_ShadeNorm(&t1,(Vec3D){ 0.5f,0.4f,0.6f,1.0f });
+			Triangle_ShadeNorm(&t2,(Vec3D){ 0.5f,0.4f,0.6f,1.0f });
+			Triangle_ShadeNorm(&t3,(Vec3D){ 0.5f,0.4f,0.6f,1.0f });
+			Triangle_ShadeNorm(&t4,(Vec3D){ 0.5f,0.4f,0.6f,1.0f });
 
 			Vector_Push(&meshCube.tris,&t1);
 			Vector_Push(&meshCube.tris,&t2);
